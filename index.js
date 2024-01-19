21fd48f456d4b2897181463d565275febcae7f3d
rabindrapradhan800 <153349968+rabindrapradhan800@users.noreply.github.com>
2024-01-19 11:52:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function myfunction(){
+    document.getElementsById(alert('Your message susscefully send. thank you'));
+} 
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
